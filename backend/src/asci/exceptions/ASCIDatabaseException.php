3ca115625aeaca2a7c26cfee3e5b8c79aed0ec51
Robbie Hott <jh2jf@virginia.edu>
2022-11-08 15:25:35 -0500

    Added exceptions and database connector for easier connection with the database

---
+++ backend/src/asci/exceptions/ASCIDatabaseException.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Database Exception Class
+ *
+ *
+ * License:
+ *
+ *
+ * @author Robbie Hott
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @copyright 2015 the Rector and Visitors of the University of Virginia, and
+ *            the Regents of the University of California
+ */
+namespace asci\exceptions;
+
+/**
+ * ASCIDatabaseException Class
+ *
+ * Exception for handling errors with ASCI back-end database connection.
+ *
+ * @author Robbie Hott
+ *
+ */
+class ASCIDatabaseException extends ASCIException {
+
+    /**
+     * Type of the exception being thrown
+     *
+     * @var string
+     */
+    protected $type = "Data Storage Error";
+}
