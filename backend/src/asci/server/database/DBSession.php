8abc1d968130def026f45ae1502446fe512b6417
markfloryan <mark.floryan@gmail.com>
2023-09-25 09:55:17 -0400

    backend issue with clear queue query

--- backend/src/asci/server/database/DBSession.php
+++ backend/src/asci/server/database/DBSession.php
@@ -178,7 +178,7 @@ class DBSession
      */
     public function closeAllSessionsForCourse($course_id){
 
-        $query = 'update sessions set status = \'completed\' from (select * from sessions JOIN session_users on id=session_id) S where S.id = sessions.id and sessions.course_id=$1 and status!=\'in_progress\'';
+        $query = 'update sessions set status = \'completed\' from (select * from sessions JOIN session_users on id=session_id) S where S.id = sessions.id and sessions.course_id=$1 and sessions.status!=\'in_progress\'';
 
         $result = $this->db->query($query, array($course_id));
         
